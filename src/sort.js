e9e125e95e4415579efa19003bd1c9374352575d
Nastia Kartseva <danetzachem@gmail.com>
2022-06-23 21:19:49 +0300

    edit sorting function syntax

--- src/sort.js
+++ src/sort.js
@@ -11,18 +11,18 @@ const ascSort = (a, b) => {
   return a.prices[0] - b.prices[0];
 };
 
-  const descSort = (a, b) => {
-    if (a.prices[0] === null && b.prices[1] === null) {
-        return b.prices[0] - a.prices[1];
-      }
-      if (b.prices[0] === null && a.prices[1] === null) {
-        return b.prices[1] - a.prices[0];
-      }
-    if (a.prices[0] === b.prices[0] || a.prices[0] === null || b.prices[0] === null) {
-        return b.prices[1] - a.prices[1];
+const descSort = (a, b) => {
+  if (a.prices[0] === null && b.prices[1] === null) {
+      return b.prices[0] - a.prices[1];
     }
-    return b.prices[0] - a.prices[0];
-  };
+  if (b.prices[0] === null && a.prices[1] === null) {
+      return b.prices[1] - a.prices[0];
+    }
+  if (a.prices[0] === b.prices[0] || a.prices[0] === null || b.prices[0] === null) {
+      return b.prices[1] - a.prices[1];
+  }
+  return b.prices[0] - a.prices[0];
+};
 
 const sortPrices = (courses, order = 'asc') => {
   const cb = order === 'desc' ? descSort : ascSort;
