5f8e2d3be16d9bbb0f31c3632098b1b5b7c20319
Nastia Kartseva <danetzachem@gmail.com>
2022-06-23 18:24:16 +0300

    add fixtures fir tests

---
+++ __fixtures__/fixtures.js
@@ -0,0 +1,59 @@
+export const courses = [
+    { name: "Courses in England", prices: [0, 100] }, 
+    { name: "Courses in Germany", prices: [500, null] }, 
+    { name: "Courses in Italy", prices: [100, 200] }, 
+    { name: "Courses in Russia", prices: [null, 400] },
+    { name: "Courses in China", prices: [50, 250] },
+    { name: "Courses in USA", prices: [200, null] },
+    { name: "Courses in Kazakhstan", prices: [56, 324] },
+    { name: "Courses in France", prices: [null, null] },
+];
+
+export const result1 = [
+    { name: "Courses in England", prices: [0, 100] }, 
+    { name: "Courses in Italy", prices: [100, 200] }, 
+    { name: "Courses in China", prices: [50, 250] },
+    { name: "Courses in USA", prices: [200, null] },
+    { name: "Courses in Kazakhstan", prices: [56, 324] },
+];
+export const result2 = [
+    { name: "Courses in England", prices: [0, 100] }, 
+    { name: "Courses in Italy", prices: [100, 200] }, 
+    { name: "Courses in China", prices: [50, 250] },
+    { name: "Courses in USA", prices: [200, null] },
+    { name: "Courses in Kazakhstan", prices: [56, 324] },
+];
+export const result3 = [
+    { name: "Courses in Germany", prices: [500, null] }, 
+    { name: "Courses in Italy", prices: [100, 200] }, 
+    { name: "Courses in Russia", prices: [null, 400] },
+    { name: "Courses in China", prices: [50, 250] },
+    { name: "Courses in USA", prices: [200, null] },
+    { name: "Courses in Kazakhstan", prices: [56, 324] },
+];
+
+export const requiredRange1 = [null, 200];
+export const requiredRange2 = [100, 350];
+export const requiredRange3 = [200, null];
+
+export const ascSortedCourses = [
+    { name: "Courses in France", prices: [null, null] },
+    { name: "Courses in England", prices: [0, 100] },
+    { name: "Courses in China", prices: [50, 250] },
+    { name: "Courses in Kazakhstan", prices: [56, 324] },
+    { name: "Courses in Italy", prices: [100, 200] },
+    { name: "Courses in USA", prices: [200, null] },
+    { name: "Courses in Russia", prices: [null, 400] },
+    { name: "Courses in Germany", prices: [500, null] }, 
+];
+
+export const descSortedCourses = [
+  { name: 'Courses in Germany', prices: [ 500, null ] },
+  { name: 'Courses in Russia', prices: [ null, 400 ] },
+  { name: 'Courses in USA', prices: [ 200, null ] },
+  { name: 'Courses in Italy', prices: [ 100, 200 ] },
+  { name: 'Courses in Kazakhstan', prices: [ 56, 324 ] },
+  { name: 'Courses in China', prices: [ 50, 250 ] },
+  { name: 'Courses in England', prices: [ 0, 100 ] },
+  { name: 'Courses in France', prices: [ null, null ] }
+];
