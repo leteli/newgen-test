d7fe95fc9ca25f5e33141b4c9c7756590c152a0a
Nastia Kartseva <danetzachem@gmail.com>
2022-06-23 21:18:47 +0300

    edit test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,18 @@
 import getRequiredCourses from '../src/index.js';
 import { courses, result1, result2, result3, requiredRange1, requiredRange2, requiredRange3 } from '../__fixtures__/fixtures.js';
 
-test('filters courses', () => {
+test('only max value', () => {
   expect(getRequiredCourses(courses, requiredRange1)).toStrictEqual(result1);
+});
+
+test('min and max value', () => {
   expect(getRequiredCourses(courses, requiredRange2)).toStrictEqual(result2);
+
+});
+
+test('only min value', () => {
+  console.log(getRequiredCourses(courses, requiredRange3));
+  console.log(result3);
   expect(getRequiredCourses(courses, requiredRange3)).toStrictEqual(result3);
+
 });
